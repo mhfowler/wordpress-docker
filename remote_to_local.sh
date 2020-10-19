a7f4d7af20ad6199bb295ece9c23f4c5752816de
Dan Taeyoung <provolot@gmail.com>
2020-10-19 00:40:25 -0400

    initial pass at adding variables and env

--- remote_to_local.sh
+++ remote_to_local.sh
@@ -1,5 +1,13 @@
-mysqldump -P 3306 -h mysql.lovelanguages.melaniehoff.com -u localdlluser lovelanguages_melaniehof -p > ~/Desktop/wp.sql
-mysql -u root -p LOCAL_DATABASE_NAME < ~/Desktop/wp.sql
-mysql -uroot -pPASSWORD LOCAL_DATABASE_NAME -e "update wp_uibgns_options set option_value='http://localhost' where option_name='siteurl'"
-mysql -uroot -pPASSWORD LOCAL_DATABASE_NAME -e "update wp_uibgns_options set option_value='http://localhost' where option_name='home'"
-rsync -avzh dll_admin@lovelanguages.melaniehoff.com:/home/dll_admin/lovelanguages.melaniehoff.com/wp-content/uploads/ ./wp-content/uploads/
+set -o allexport
+source ".env"
+set +o allexport
+
+tmpfile=$(mktemp)
+
+#mysqldump -P $REMOTE_MYSQL_PORT -h $REMOTE_MYSQL_HOST -u $REMOTE_MYSQL_USER $REMOTE_MYSQL_DB --password=$REMOTE_MYSQL_PASSWORD > $tmpfile
+
+#docker exec -i CONTAINER /usr/bin/mysql -u root --password=root DATABASE < backup.sql
+#mysql -u root -p $LOCAL_DATABASE_NAME < $tmpfile
+#mysql -uroot -pPASSWORD $LOCAL_DATABASE_NAME -e "update wp_uibgns_options set option_value='http://localhost' where option_name='siteurl'"
+#mysql -uroot -pPASSWORD $LOCAL_DATABASE_NAME -e "update wp_uibgns_options set option_value='http://localhost' where option_name='home'"
+#rsync -avzh dll_admin@lovelanguages.melaniehoff.com:/home/dll_admin/lovelanguages.melaniehoff.com/wp-content/uploads/ ./wp-content/uploads/
